dc11324cd94b9b570a30f1af3f57dd14eac2656b
baba-beda <darya.nightmare@gmail.com>
2014-10-04 21:08:42 +0400

    everything is passed

--- src/biconv.java
+++ src/biconv.java
@@ -31,52 +31,84 @@ public class biconv {
         }
     }
 
+    class Pair {
+        int first;
+        int second;
+        Pair (int a, int b) {
+            first = a;
+            second = b;
+        }
+    }
 
     FastScanner in;
     PrintWriter out;
-    ArrayList<Integer>[] graph;
+    ArrayList<Pair>[] graph;
     int[] colors, fup, timeIn;
     int maxColor;
     boolean[] visited;
-    ArrayList<Integer> ans;
     Stack<Integer> stack;
     int timer;
 
     public void solve() throws IOException {
         int n = in.nextInt(), m = in.nextInt();
         graph = new ArrayList[n];
-        colors = new int[n];
+        colors = new int[m];
         visited = new boolean[n];
         fup = new int[n];
         timeIn = new int[n];
-        ans = new ArrayList<Integer>();
         stack = new Stack<Integer>();
 
         for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<Integer>();
+            graph[i] = new ArrayList<Pair>();
         }
 
         for (int i = 0; i < m; i++) {
             int a = in.nextInt() - 1, b = in.nextInt() - 1;
-            graph[a].add(b);
-            graph[b].add(a);
+            graph[a].add(new Pair (b, i));
+            graph[b].add(new Pair(a, i));
         }
         maxColor = 0;
         Arrays.fill(colors, 0);
         timer = 0;
         for (int i = 0; i < n; i++) {
-            if (!visited[i])
+            if (!visited[i]) {
                 dfs(i, -1);
+            }
         }
         out.println(maxColor);
         for (int k : colors) {
             out.print(k + " ");
         }
+
     }
 
     void dfs(int u, int p) {
-
+        visited[u] = true;
+        fup[u] = timeIn[u] = timer++;
+        for (Pair v : graph[u]) {
+            if (v.first == p)
+                continue;
+            if (!visited[v.first]) {
+                stack.push(v.second);
+                dfs(v.first, u);
+                if (fup[v.first] >= timeIn[u]) {
+                    maxColor++;
+                    while (stack.peek() != v.second) {
+                        colors[stack.pop()] = maxColor;
+                    }
+                    colors[stack.pop()] = maxColor;
+                }
+                fup[u] = Math.min(fup[u], fup[v.first]);
+            }
+            else {
+                if (timeIn[v.first] < timeIn[u])
+                    stack.push(v.second);
+                if (fup[u] > timeIn[v.first])
+                    fup[u] = timeIn[v.first];
+            }
+        }
     }
+
 
 
     public void run() {
